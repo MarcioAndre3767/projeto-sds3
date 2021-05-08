a29278d80bdfad5af5cc0553c1d331411f2b22ae
Marcio Andre Mendes <marcioandre.ti1@gmail.com>
2021-05-08 16:27:07 -0300

    alteração no requests

--- frontend/src/utils/requests.ts
+++ frontend/src/utils/requests.ts
@@ -1,1 +1,7 @@
 export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080';
+
+
+/*
+No Netlify, variáveis de ambiente com prefixo "REACT_APP" 
+serão porcessadas pelo browser e o Javascript terá acesso as mesmas
+*/
